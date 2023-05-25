f62a0f1962d7ac3c1bc8c07a0131b04c00ce3c5c
Samu Toljamo <samu.toljamo@avanio.com>
2023-05-25 14:17:27 +0300

    initial

---
+++ test/index.test.ts
@@ -0,0 +1,14 @@
+import 'mocha';
+import * as chai from 'chai';
+import { hello } from '../src';
+
+const expect = chai.expect;
+
+
+describe('index.ts', () => {
+  describe('hello', () => {
+    it('should return world', () => {
+      expect(hello()).to.be.equal('world');
+    })
+  })
+})
