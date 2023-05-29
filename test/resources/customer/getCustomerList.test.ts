761c45217dd0d44e505f2791ca0cb658c520615e
Samu Toljamo <samu.toljamo@avanio.com>
2023-05-29 11:53:08 +0000

    Add unit test

---
+++ test/resources/customer/getCustomerList.test.ts
@@ -0,0 +1,30 @@
+import 'mocha';
+import * as chai from 'chai';
+import * as sinon from 'sinon';
+import {IApiProvider, resources} from '../../../src';
+
+const expect = chai.expect;
+
+const apiSpy = sinon.spy();
+const handleRequestSpy = sinon.spy();
+
+describe('customer', () => {
+	describe('getCustomerList', () => {
+		it('should call api', async () => {
+			const api: IApiProvider = {
+				request: apiSpy,
+				handleRequest: handleRequestSpy,
+			};
+			await resources.customer.getCustomerList(api);
+			expect(apiSpy.calledOnce).to.be.equal(true);
+			expect(
+				apiSpy.calledWith({
+					method: 'GET',
+					params: undefined,
+					parse: sinon.match.func,
+					path: '/customerlist.nv',
+				}),
+			).to.be.equal(true);
+		});
+	});
+});
