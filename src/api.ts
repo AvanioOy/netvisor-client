8df103b5ef431865c6db9bf86375fda01b7bf700
Samu Toljamo <samu.toljamo@avanio.com>
2023-06-06 08:30:12 +0000

    added working implementation for customerlist

--- src/api.ts
+++ src/api.ts
@@ -9,15 +9,19 @@ export type NetvisorRequest<T, U> = {
 	body?: string;
 };
 
+export const netvisorEnvironment = {
+	isv: 'https://isvapi.netvisor.fi',
+} as const;
+
 export interface IConfig {
 	client: string;
 	userKey: string;
 	partnerKey: string;
 	language: string;
 	organisationId: string;
-	baseUrl: string;
 	privateKey: string;
 	partnerPrivateKey: string;
+	env: keyof typeof netvisorEnvironment;
 }
 
 export interface IConfigProvider {
@@ -33,6 +37,44 @@ export class ConfigProvider implements IConfigProvider {
 	getConfig = async () => this.config;
 }
 
+export class EnviromentConfigProvider implements IConfigProvider {
+	getConfig: IConfig;
+	constructor() {
+		if (process.env.NETVISOR_ENV == null || Object.keys(netvisorEnvironment).includes(process.env.NETVISOR_ENV) === false) {
+			throw new Error(
+				`NETVISOR_ENV is not set or is invalid: It is ${process.env.NETVISOR_ENV} but should be one of ${Object.keys(netvisorEnvironment).join(', ')}`,
+			);
+		}
+		if (process.env.NETVISOR_CLIENT == null) {
+			console.warn('NETVISOR_CLIENT is not set, using default "default"');
+		}
+
+		const fieldsToCheck = [
+			'NETVISOR_USER_KEY',
+			'NETVISOR_PARTNER_KEY',
+			'NETVISOR_LANGUAGE',
+			'NETVISOR_ORGANISATION_ID',
+			'NETVISOR_PRIVATE_KEY',
+			'NETVISOR_PARTNER_PRIVATE_KEY',
+		];
+		for (const field of fieldsToCheck) {
+			if (process.env[field] == null) {
+				throw new Error(`${field} is not set`);
+			}
+		}
+		this.getConfig = {
+			client: process.env.NETVISOR_CLIENT ?? 'default',
+			env: process.env.NETVISOR_ENV as keyof typeof netvisorEnvironment,
+			language: process.env.NETVISOR_LANGUAGE ?? '',
+			organisationId: process.env.NETVISOR_ORGANISATION_ID ?? '',
+			partnerKey: process.env.NETVISOR_PARTNER_KEY ?? '',
+			partnerPrivateKey: process.env.NETVISOR_PARTNER_PRIVATE_KEY ?? '',
+			privateKey: process.env.NETVISOR_PRIVATE_KEY ?? '',
+			userKey: process.env.NETVISOR_USER_KEY ?? '',
+		};
+	}
+}
+
 export interface IApiProvider {
 	handleRequest(url: string, params: Record<string, string>, method: string, body: string): Promise<Request>;
 	request<T, U>(request: NetvisorRequest<T, U>): Promise<T | null>;
@@ -40,27 +82,25 @@ export interface IApiProvider {
 
 export class ApiProvider implements IApiProvider {
 	private configProvider = {} as IConfigProvider;
-	private config = {} as IConfig;
 	constructor(configProvider: IConfigProvider) {
 		this.configProvider = configProvider;
 	}
 
 	public async request<T, U>(request: NetvisorRequest<T, U>): Promise<T | null> {
-		await this.ensureConfig();
-		const url = `${this.config.baseUrl}${request.path}`;
+		const config = await (typeof this.configProvider.getConfig === 'function' ? this.configProvider.getConfig() : this.configProvider.getConfig);
+		const url = `${netvisorEnvironment[config.env]}${request.path}`;
 		const withAuth = await this.handleRequest(url, request.params ?? {}, request.method, request.body || '');
-
 		const response = await fetch(withAuth);
 		if (!response.ok) throw new Error(response.statusText);
 		return request.parse ? request.parse(response) : null;
 	}
 
 	public async handleRequest(url: string, params: Record<string, string>, method: string, body: string): Promise<Request> {
-		await this.ensureConfig();
-		const transId = `TRANSID${Math.random() * 100000000000000000}`;
+		const config = await (typeof this.configProvider.getConfig === 'function' ? this.configProvider.getConfig() : this.configProvider.getConfig);
+		const transId = `${Math.random() * 100000000000000000}`;
 		const timestamp = new Date().toISOString();
 
-		if (params) {
+		if (params && Object.keys(params).length > 0) {
 			const paramsString = Object.keys(params)
 				.map((key) => `${key}=${params[key]}`)
 				.join('&');
@@ -68,35 +108,25 @@ export class ApiProvider implements IApiProvider {
 		}
 		const request = new Request(url, {
 			method,
-			body,
+			body: method === 'POST' || method === 'PUT' ? body : undefined,
 		});
 
-		request.headers.set('X-Netvisor-Application-Sender', this.config.client);
-		request.headers.set('X-Netvisor-Authentication-CustomerId', this.config.userKey);
-		request.headers.set('X-Netvisor-Partner-Id', this.config.partnerKey);
-		request.headers.set('X-Netvisor-Language', this.config.language);
-		request.headers.set('X-Netvisor-Organisation-ID', this.config.organisationId);
+		request.headers.set('X-Netvisor-Authentication-Sender', config.client);
+		request.headers.set('X-Netvisor-Authentication-CustomerId', config.userKey);
+		request.headers.set('X-Netvisor-Authentication-PartnerId', config.partnerKey);
+		request.headers.set('X-Netvisor-Interface-Language', config.language);
+		request.headers.set('X-Netvisor-Organisation-ID', config.organisationId);
 		request.headers.set('X-Netvisor-Authentication-Timestamp', timestamp);
 		request.headers.set('X-Netvisor-Authentication-TransactionId', transId);
 		await this.calculateMac(request, transId, timestamp);
 		return request;
 	}
 
-	private async ensureConfig() {
-		if (!this.config) {
-			this.config = await (typeof this.configProvider.getConfig === 'function' ? this.configProvider.getConfig() : this.configProvider.getConfig);
-		}
-		if (!this.config) {
-			throw new Error('No config found');
-		}
-	}
-
 	private async calculateMac(request: Request, transId: string, timestamp: string) {
-		await this.ensureConfig();
-		const config = this.config;
-		const macString = `${request.url}&${config.client}&${config.userKey}&${timestamp}&${config.language}&${transId}&${config.privateKey}&${config.partnerPrivateKey}`;
+		const config = await (typeof this.configProvider.getConfig === 'function' ? this.configProvider.getConfig() : this.configProvider.getConfig);
+		const macString = `${request.url}&${config.client}&${config.userKey}&${timestamp}&${config.language}&${config.organisationId}&${transId}&${config.privateKey}&${config.partnerPrivateKey}`;
 		const mac = crypto.createHash('sha256').update(macString).digest('hex');
 		request.headers.set('X-Netvisor-Authentication-MAC', mac);
-		request.headers.set('X-Netvisor-Partner-MACHashCalculationAlgorithm', 'SHA256');
+		request.headers.set('X-Netvisor-Authentication-MACHashCalculationAlgorithm', 'SHA256');
 	}
 }
